853b9637152458d26819ed6287e0273e30a1de98
rqueztech <rqueztech@noreply.com>
2024-04-26 23:13:31 +0900

    Change CSV operations.

--- cmd/csvoperations.go
+++ cmd/csvoperations.go
@@ -22,16 +22,16 @@ func WriteCSV() {
 }
 */
 
-func ReadCSV() (string, error) {
+func ReadCSV() (string) {
      // file variable -> recieves pointer of csv file if found
     // err -> kicks error if this is not found.
     file, err := os.Open("resources/times.csv")
 
     // If the error is not null, meaning an error was found... then print out custom error. er is verbose, so we replace with our own
     if(err != nil) {
-        fmt.Println("File Found...")
-    } else {
         fmt.Println("File Not Found...")
+    } else {
+        fmt.Println("File Found...")
     }
 
     // Create buffer to aggregate all input
@@ -57,5 +57,5 @@ func ReadCSV() (string, error) {
         }
     }
 
-    return buffer.String(), nil
+    return buffer.String()
 }
