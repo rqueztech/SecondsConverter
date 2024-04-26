754df29ffc5746abd9cc9d94fe2b97f0896f141a
rqueztech <rqueztech@noreply.com>
2024-04-26 11:09:51 +0900

    Write the file checker in both the read and write functions, splitting the check will be more redundant.

--- cmd/csvoperations.go
+++ cmd/csvoperations.go
@@ -5,7 +5,7 @@ import (
     "fmt";
 )
 
-func CheckForCSV() bool {
+func Read CSV() bool {
      // file variable -> recieves pointer of csv file if found
     // err -> kicks error if this is not found.
     file, err := os.Open("resources/times.csv")
@@ -20,10 +20,18 @@ func CheckForCSV() bool {
     }   
 }
 
-func ReadCSV() {
-    CheckForCSV()
-}
 
-func WriteCSV() {
-    CheckForCSV()
+func WriteCSV() bool {
+     // file variable -> recieves pointer of csv file if found
+    // err -> kicks error if this is not found.
+    file, err := os.Open("resources/times.csv")
+
+    // If the error is not null, meaning an error was found... then print out custom error. er is verbose, so we replace with our own
+    if(err != nil) {
+        fmt.Println("File Found...")
+        return true
+    } else {
+        fmt.Println("File Not Found...")
+        return false
+    }   
 }
