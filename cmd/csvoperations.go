d70b4b0402c574fd0ec3d41dc12b8eb38ffa36bc
rqueztech <rqueztech@noreply.com>
2024-04-26 10:48:49 +0900

    Initialize the main file.

---
+++ cmd/csvoperations.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "os";
+    "fmt";
+)
+
+func CheckForCSV()  {
+     // file variable -> recieves pointer of csv file if found
+    // err -> kicks error if this is not found.
+    file, err := os.Open("resources/times.csv")
+
+    // If the error is not null, meaning an error was found... then print out custom error. er is verbose, so we replace with our own
+    if(err != nil) {
+        fmt.Println("File Found...")
+    } else {
+        fmt.Println("File Not Found...")
+    }   
+}
---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt";
+)
+
+func main(){
+    CheckForCSV()
+    fmt.Println("Here we go")
+}
