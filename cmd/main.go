d77dbdb7a2d6da30f92417316e2d701acb7af9ff
rqueztech <rqueztech@noreply.com>
2024-04-27 19:35:55 +0900

    Update to read in link name and properly format times for example link.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "fmt";
-    "strings";
-    "strconv";
-    "bufio";
-    "os";
+    "fmt";      // for printing and formatting
+    "strings";  // for string manipulations
+    "strconv";  // typecasting and mainuplation of strings
+    "bufio";    // for reading input from strings
+    "os";       // for all io operations
 )
 
 // Create main menu for user prompt
@@ -18,34 +18,33 @@ func mainmenu() {
 func main(){
     // print out the main menu here
     mainmenu()
+
     // Create a builder to compile the seconds
     reader := bufio.NewReader(os.Stdin)
+    linkname, err := reader.ReadString('\n')
 
-    // scan in user input
-    option, err := reader.ReadString('\n')
-
-    // Handle error for user scan
+    // check reader for errors
     if err != nil {
-        fmt.Println("No error")
-    } else {
-        fmt.Println("Error exists")
+        fmt.Println("Error reading")
     }
 
-    fmt.Println(option)
+    // trim space to handle issue with newreader reading in the newline
+    linkname = strings.TrimSpace(linkname)
 
+    // read the csv
     readValues := ReadCSV()
-    //WriteCSV()
 
+    // check if the data exists (may be redundant)
     if readValues != "" {
         fmt.Println("Data exits")
     } else {
         fmt.Println("Data does not exist")
     }
-    
 
-    fmt.Println(readValues)
+    // split the time values here
     thisis := strings.Split(readValues, ",")
 
+    // iterate through every time that was put in the csv
     for _,currentTime := range thisis {
         // handles extra garbage character at end
         if len(currentTime) > 1 {
@@ -59,9 +58,9 @@ func main(){
 
             totalseconds := (hours*3600) + (minutes*60) + (seconds)
 
-            fmt.Println(currentTime, ",", totalseconds)
+            fmt.Printf("<a href = \"%s&t=%ds\"></a>\n", linkname, totalseconds)
         }
     }
 
-    fmt.Println("end")
+    fmt.Println("\nend")
 }
