d544b83e5e7c6b044758378426f2360b6c25be15
rqueztech <rqueztech@noreply.com>
2024-05-02 21:14:14 +0900

    Separate functions for later cleaning, ensure code can be tested.

--- cmd/main.go
+++ cmd/main.go
@@ -8,39 +8,10 @@ import (
     "os";       // for all io operations
 )
 
-func main(){
-    fmt.Print("Enter the link name: ")
-
-    // Create a builder to compile the seconds
-    reader := bufio.NewReader(os.Stdin)
-    linkname, err := reader.ReadString('\n')
-
-    // check reader for errors
-    if err != nil {
-        fmt.Println("Error reading")
-    }
-
-    // trim space to handle issue with newreader reading in the newline
-    linkname = strings.TrimSpace(linkname)
-
-    // read the csv
-    readValues := ReadCSV()
-
-    // check if the data exists (may be redundant)
-    if readValues != "" {
-        fmt.Println("Data exits")
-    } else {
-        fmt.Println("Data does not exist")
-    }
-
-    // split the time values here
-    timestosplit := strings.Split(readValues, ",")
-
-    // make an array at the length of timesplit.
+func timesplitter(linkname string, timestosplit []string) [][]string { 
     writearray := make([][]string, len(timestosplit))
-
     currentvalue := 0
-    
+
     // iterate through every time that was put in the csv
     for index,currentTime := range timestosplit {
         // handles extra garbage character at end
@@ -63,12 +34,52 @@ func main(){
         }
     }
 
-    // stopping point: array prints right
+    writeCSV(writearray)
+
+    return writearray
+}
+
+func displayarray(writearray [][]string) {
+     // stopping point: array prints right
     for _, col := range(writearray) {
         for _, row := range(col) {
             fmt.Println(row)
         }
+    }   
+}
+
+func main(){
+    var times_csv string = "resources/times.csv"
+
+    fmt.Print("Enter the link name: ")
+
+    // Create a builder to compile the seconds
+    reader := bufio.NewReader(os.Stdin)
+    linkname, err := reader.ReadString('\n')
+
+    // check reader for errors
+    if err != nil {
+        fmt.Println("Error reading")
     }
 
-    writeCSV(writearray)
+    // trim space to handle issue with newreader reading in the newline
+    linkname = strings.TrimSpace(linkname)
+
+    // read the csv
+    var readValues string = ReadCSV(times_csv)
+
+    // check if the data exists (may be redundant)
+    if readValues != "" {
+        fmt.Println("Data exits")
+    } else {
+        fmt.Println("Data does not exist")
+    }
+
+    // split the time values here
+    var timestosplit []string = strings.Split(readValues, ",")
+
+    // make an array at the length of timesplit.
+    var writearray [][]string = timesplitter(linkname, timestosplit)
+
+    displayarray(writearray)
 }
